5449196dd92429b36d33ed317abe5bec825e3ab6
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-02-01 22:47:01 +0530

    initialized the app and made the animated divs

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+
+function App() {
+
+  const gridContainerVarients = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { staggerChildren: 0.25 }, },
+  }
+
+  const gridSquareVarients = {
+    hidden : {opacity : 0}, show: {opacity : 1}
+  }
+
+  return (
+    <div className="flex flex-col gap-10 overflow-x-hidden">
+      <motion.section
+        variants={gridContainerVarients}
+        initial="hidden"
+        animate="show"
+        className="grid grid-cols-3 p-10 gap-10"
+      >
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+      </motion.section>
+    </div>
+  );
+}
+
+export default App;
