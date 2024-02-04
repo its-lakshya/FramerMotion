a8337d637caf28fed379183b5604bf28236c3b8d
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-02-04 15:18:02 +0530

    implemented second animation

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,20 @@ function App() {
           </motion.div>
         </motion.div>
 
-        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10">
+          <motion.div 
+          className='w-1/3 h-1/3 shadow-md bg-rose-400'
+          animate={{
+            scale:[1,2,2,1],
+            rotate:[0, 90, 90, 0],
+            borderRadius:["10%", "10%", "50%", '10%'],
+          }}
+          transition={{duration: 5, ease:"easeInOut", repeat:Infinity, repeatDelay:1}}
+          >
+
+          </motion.div>
+
+        </motion.div>
         <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
         <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
         <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
