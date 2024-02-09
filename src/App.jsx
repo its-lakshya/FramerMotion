8eb87f34f3ca33c8fb11f7d8ecbba3708a0056a7
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-02-09 21:49:55 +0530

    implemented the fourth animation

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,11 @@ function App() {
         </motion.div>
         
         
-        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10">
+
+          <motion.div className='w-1/3 h-1/3 bg-orange-500 rounded-3xl cursor-grap' drag dragConstraints={{top:-125, right:125, bottom:125, left:-125}} dragTransition={{bounceStiffness:600, bounceDamping:10}}></motion.div>
+
+        </motion.div>
         <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
         <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
       
