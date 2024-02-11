f713f4c33139eff8c0273191a766949edc5d78da
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-02-11 23:54:19 +0530

    implemented the fifth animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 
 function App() {
+
+  const {scrollYProgress: completionProgress} = useScroll();
 
   const gridContainerVarients = {
     hidden: { opacity: 0 },
@@ -62,7 +64,16 @@ function App() {
           <motion.div className='w-1/3 h-1/3 bg-orange-500 rounded-3xl cursor-grap' drag dragConstraints={{top:-125, right:125, bottom:125, left:-125}} dragTransition={{bounceStiffness:600, bounceDamping:10}}></motion.div>
 
         </motion.div>
-        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+
+        <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10">
+          <motion.div className="w-40 aspect-square bg-gray-50/20">
+            <motion.div className="w-full bg-gray-400 rounded-xl h-full origin-bottom" 
+              style={{scaleY: completionProgress}}>
+            </motion.div>
+          </motion.div>
+
+        </motion.div>
+
         <motion.div variants={gridSquareVarients} className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
       
       </motion.section>
